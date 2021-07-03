303ecaa53ef31109a4c0982eabae3ebb3e112a76
Juan Apolinar <juanapolinar@my.unt.edu>
2021-07-03 13:25:20 -0500

    adding project to git

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDu8N1v-FrE-SsMy4wVCQ9wZ6PjDNyQsLE",
+    authDomain: "kanban-fire-workshop-57843.firebaseapp.com",
+    projectId: "kanban-fire-workshop-57843",
+    storageBucket: "kanban-fire-workshop-57843.appspot.com",
+    messagingSenderId: "733731802308",
+    appId: "1:733731802308:web:388d1ae3bc5d51e562cd91",
+    measurementId: "G-BNMS3WXSE8"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDu8N1v-FrE-SsMy4wVCQ9wZ6PjDNyQsLE",
+    authDomain: "kanban-fire-workshop-57843.firebaseapp.com",
+    projectId: "kanban-fire-workshop-57843",
+    storageBucket: "kanban-fire-workshop-57843.appspot.com",
+    messagingSenderId: "733731802308",
+    appId: "1:733731802308:web:388d1ae3bc5d51e562cd91",
+    measurementId: "G-BNMS3WXSE8"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
